848f43c6bf9949cfe2016a3752837d9ade9512a4
Hui Wang <huiwang@HuimatoMacBook-Pro.local>
2018-02-04 16:44:59 -0500

    Integrating Spring Boot with MySQL

---
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018MaApplication.java
@@ -0,0 +1,24 @@
+package edu.northeastern.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Cs5200Spring2018MaApplication extends SpringBootServletInitializer{
+	 @Override
+
+	 protected SpringApplicationBuilder
+
+	  configure(SpringApplicationBuilder application) {
+
+	  return application.sources(
+
+	   Cs5200Spring2018MaApplication.class);
+
+	 }
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Spring2018MaApplication.class, args);
+	}
+}
